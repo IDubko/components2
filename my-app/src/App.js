6093478b38727e97c3c898b59cae54a889728913
IDubko <dubkoivl@yandex.com>
2024-10-18 17:22:22 +0700

    final2  develop

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,6 @@ export const App = () => {
 	const [steps] = useState(data.steps); // Извлекаем шаги из JSON файла
     const [activeIndex, setActiveIndex] = useState(0); // Устанавливаем начальный шаг на 0
 
-
 	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
 
 	// Обработчик для клика "Назад"
@@ -37,52 +36,54 @@ export const App = () => {
 
 	return (
 		<div className={styles.container}>
-		  <div className={styles.card}>
-			<h1>Инструкция по готовке пельменей</h1>
-			<div className={styles.steps}>
-			  <div className={styles['steps-content']}>
-				{/* Выводим контент текущего шага */}
-				{steps[activeIndex].content}
-			  </div>
-			  <ul className={styles['steps-list']}>
-				{steps.map((step, index) => (
-				  <li
-					key={index}
-					className={[
-					  styles['steps-item'],
-					  index < activeIndex ? styles.done : '',
-					  index === activeIndex ? styles.active : ''
-					].join(' ')}
-				  >
-					<button
-					  className={styles['steps-item-button']}
-					  onClick={() => setActiveIndex(index)} // Устанавливаем активный шаг при клике
-					>
-					  {index + 1}
-					</button>
-					Шаг {index + 1}
-				  </li>
-				))}
-			  </ul>
-			  <div className={styles['buttons-container']}>
-				<button
-				  className={styles.button}
-				  onClick={handleBackClick}
-				  disabled={isFirstStep} // Дизактивируем кнопку, если находимся на первом шаге
-				>
-				  Назад
-				</button>
-				<button
-				  className={styles.button}
-				  onClick={isLastStep ? handleRestartClick : handleNextClick}
-				>
-				  {isLastStep ? 'Начать сначала' : 'Далее'}
-				</button>
-			  </div>
-			</div>
-		  </div>
-		</div>
-	  );
-	};
-	
-	export default App;
+      <div className={styles.card}>
+        <h1>Инструкция по готовке пельменей</h1>
+        <div className={styles.steps}>
+          <div className={styles['steps-content']}>
+            {/* Выводим контент текущего шага */}
+            {steps[activeIndex].content}
+          </div>
+          <ul className={styles['steps-list']}>
+            {steps.map((step, index) => {
+              // Определяем классы для каждого элемента
+              const itemClasses = [
+                styles['steps-item'],
+                (index < activeIndex) ? styles.done : '',
+                (index === activeIndex) ? styles.active : ''
+              ].join(' ');
+
+              return (
+                <li key={index} className={itemClasses}>
+                  <button
+                    className={styles['steps-item-button']}
+                    onClick={() => setActiveIndex(index)} // Устанавливаем активный шаг при клике
+                  >
+                    {index + 1}
+                  </button>
+                  Шаг {index + 1}
+                </li>
+              );
+            })}
+          </ul>
+          <div className={styles['buttons-container']}>
+            <button
+              className={styles.button}
+              onClick={handleBackClick}
+              disabled={isFirstStep} // Дизактивируем кнопку, если находимся на первом шаге
+            >
+              Назад
+            </button>
+            <button
+              className={styles.button}
+              onClick={isLastStep ? handleRestartClick : handleNextClick}
+            >
+              {isLastStep ? 'Начать сначала' : 'Далее'}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
